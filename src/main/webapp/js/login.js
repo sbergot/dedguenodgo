49271bd9d6a296517edd4dbcb83135ab377e48ac
oadam <oadam@efront.com>
2014-01-31 22:56:09 +0000

    login work in progress

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -1,17 +1,39 @@
-window.ViewModel = function() {
-	this.partyId = ko.observable();
-	this.partyPassword = ko.observable();
-	this.userId = ko.observable();
-	this.loadingParty = ko.observable(false);
-	this.partyOk = ko.observable(false);
-	this.loadingUser = ko.observable(false);
-	this.userOk = ko.observable(false);
+(function() {
+	function createStorageObservable(storage, propertyName) {
+		var storedValue = null;
+		try {
+			storedValue = JSON.parse(storage.getItem(propertyName));
+		} catch (e) {
+			console.warn('corrupted value for ' + propertyName, e);
+		}
+		var result = ko.observable(storedValue);
+		result.subscribe(function(value) {
+			storage.setItem(propertyName, JSON.stringify(value));
+		});
+		return result;
+	}
 
-};
+	window.LoginViewModel = function(server) {
+		this._server = server;
 
-window.ViewModel.prototype = {
-	submit: function() {},
-	addUser: function() {},
-	deleteUser: function() {}
-};
+		this.partyId = createStorageObservable(localStorage, 'partyId');
+		this.partyPassword = createStorageObservable(sessionStorage, 'partyPassword');
+		this.userIdChoice = createStorageObservable(localStorage, 'login-userIdChoice');
 
+		this.loadingParty = ko.observable(false);
+		this.partyOk = ko.observable(false);
+		this.loadingUser = ko.observable(false);
+		this.userOk = ko.observable(false);
+	};
+
+	window.LoginViewModel.prototype = {
+		submitParty: function() {
+		
+		},
+		submitUser: function() {
+		
+		},
+		addUser: function() {},
+		deleteUser: function() {}
+	};
+})();
